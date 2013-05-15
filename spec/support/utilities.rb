ba90d45688214f813bfdd56b5cc93b948c5aed4e
Carlos Feliciano <c.feliciano2009@gmail.com>
2013-05-15 10:02:25 -0400

    Changes some titles

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,5 @@
 def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
+  base_title = "CloudGame, a game database that updates automatically"
   if page_title.empty?
     base_title
   else
